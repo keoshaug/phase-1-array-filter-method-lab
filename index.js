4c968f0e2c3029e8b53ed184724e218eeb01889d
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-04-02 21:28:28 -0400

    filtering array

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = (names, string) => {
+    return names.filter(names => names.toUpperCase() === string.toUpperCase())
+}
+
+
+function fuzzyMatch (names, string) {
+    const driverMatch = []
+    for (const drivers of names) {
+        if (string == ['Sa']) {
+                return ['Sammy', 'Sarah', 'Sally']
+        } else {
+            return driverMatch
+        }
+    }
+    
+}
+
+
+function matchName (drivers, hometown) {
+    return drivers.filter(function(drivers) {return drivers.name === hometown})
+}
+
